ad60e23423aa40ff625a386e1428e0b94942a1b7
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-01-10 14:33:39 +0100

    act toggle sin acabar

--- src/controladora/ControladoraVentana1.java
+++ src/controladora/ControladoraVentana1.java
@@ -1,12 +1,15 @@
 package controladora;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
+import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -19,29 +22,74 @@ public class ControladoraVentana1 implements Initializable {
     Button btnNormal, btnImagen;
     @FXML
     Tab tabBotones, tabTextos;
+    @FXML
+    CheckBox check;
+
+    @FXML
+    RadioButton radio1, radio2, radio3;
+
+    ToggleGroup grupoRadios;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        instancias();
         acciones();
+        personalizarBotones();
 
+    }
+
+    private void instancias() {
+        grupoRadios = new ToggleGroup();
+        grupoRadios.getToggles().addAll(radio1, radio2, radio3);
+    }
+
+    private void personalizarBotones() {
+        ImageView imageView = new ImageView(new Image(getClass().getResourceAsStream("../resources/button_ok.png")));
+        btnImagen.setGraphic(imageView);
+        btnImagen.setGraphic(imageView);
     }
 
     private void acciones() {
         btnNormal.setOnMouseEntered(new manejoRaton());
-
+        btnImagen.setOnMousePressed(new manejoRaton());
         btnNormal.setOnMouseExited(new manejoRaton());
-
-
+        btnImagen.setOnMouseReleased(new manejoRaton());
+    check.selectedProperty().addListener(new ChangeListener<Boolean>() {
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if(newValue){
+                btnNormal.setDisable(true);
+            }else{
+                btnNormal.setDisable(false);
+            }
         }
+    });
+        btnNormal.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println(check.isSelected());
+                grupoRadios.getSelectedToggle();
+            }
+        });
+
+
+    }
+
     class manejoRaton implements EventHandler<MouseEvent> {
         @Override
         public void handle(MouseEvent event) {
-            if(event.getSource()== btnNormal){
-                if(event.getEventType()== MouseEvent.MOUSE_ENTERED){
+            if (event.getSource() == btnNormal) {
+                if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
                     btnNormal.setEffect(sombra);
-                }else if(event.getEventType()== MouseEvent.MOUSE_EXITED){
+
+                } else if (event.getEventType() == MouseEvent.MOUSE_EXITED) {
                     btnNormal.setEffect(null);
+                }
+            } else if (event.getSource() == btnImagen) {
+                if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+                    btnImagen.setEffect(sombra);
+                } else if (event.getEventType() == MouseEvent.MOUSE_RELEASED) {
+                    btnImagen.setEffect(null);
                 }
             }
         }
